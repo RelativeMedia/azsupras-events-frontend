43803dcea40ffbf9995efc026462837594e42785
Mike DeVita <m.devita@gmail.com>
2015-05-12 16:25:06 -0700

    stripping console.log out

--- src/app/checkout/checkout.controller.js
+++ src/app/checkout/checkout.controller.js
@@ -34,7 +34,6 @@ angular.module('frontend')
             }else{
               Cart.clear();
               $state.go('checkoutSuccess');
-              console.log(err, result);
             }
           });
         }
